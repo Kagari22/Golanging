6fc1caf7dd838ad3c2aa22056c0127b79dc3905a
Kagari22 <151036819+Kagari22@users.noreply.github.com>
2026-02-22 19:56:39 +0800

    Implement channel synchronization example in Go

---
+++ GoChannel/ChannelSync1.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var data []byte
+var idx int
+
+func nextInt() int {
+    sign := 1
+    for idx < len(data) && (data[idx] == ' ' || data[idx] == '\n' || data[idx] == '\r' || data[idx] == '\t') {
+        idx++
+    }
+    if idx < len(data) && data[idx] == '-' {
+        sign = -1
+        idx++
+    }
+    num := 0
+    for idx < len(data) && data[idx] >= '0' && data[idx] <= '9' {
+        num = num*10 + int(data[idx]-'0')
+        idx++
+    }
+    return sign * num
+}
+
+func Channel() {
+    ch := make(chan int)
+    wg := sync.WaitGroup{}
+
+    wg.Add(1)
+    go func() {
+        defer wg.Done()
+        for i := 0; i < 5; i++ {
+            ch <- i
+            fmt.Println("Send ", i, "\tNow:", time.Now().Format("15:04:05.999999999"))
+            time.Sleep(time.Second)
+        }
+        close(ch)
+    }()
+
+    wg.Add(1)
+    go func() {
+        defer wg.Done()
+        for e := range ch {
+            fmt.Println("Received ", e, "\tNow:", time.Now().Format("15:04:05.999999999"))
+            time.Sleep(time.Second)
+        }
+    }()
+
+    wg.Wait()
+}
+
+func solve() {
+    Channel()
+}
+
+func main() {
+    t := 1
+    for ; t > 0; t-- {
+        solve()
+    }
+}
