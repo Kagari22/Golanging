4b648b3d87e406360a545325b0affeda7b4f130b
Kagari22 <151036819+Kagari22@users.noreply.github.com>
2026-02-27 18:14:24 +0800

    Add NilChannel.go with channel and goroutine example

---
+++ GoChannel/NilChannel.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func Channel() {
+    ch := make(chan int) 
+
+    go func() {
+        for {
+            ch <- rand.Intn(10)
+            time.Sleep(500 * time.Millisecond)
+        }
+    }()
+
+    go func() {
+        sum := 0
+        t := time.After(5 * time.Second)
+        for {
+            select {
+            case v := <-ch:
+                fmt.Println("Received value: ", v)
+                sum += v
+            case <-t:
+                ch = nil
+                fmt.Println("Ch was set nil, sum is: ", sum)
+            }
+        }
+    }()
+
+    time.Sleep(5 * time.Second)
+}
+
+func solve() {
+    Channel()
+}
+
+func main() {
+    t := 1
+    for ; t > 0; t-- {
+        solve()
+    }
+}
