e974585a0788e6cb132607cdff82a20f2a223935
Kagari22 <151036819+Kagari22@users.noreply.github.com>
2026-06-15 16:25:01 +0800

    Update BroadcastWaitDemo.go

--- GoChannel/BroadcastWaitDemo.go
+++ GoChannel/BroadcastWaitDemo.go
@@ -20,6 +20,8 @@ func solve() {
 			// Wait() 返回后不会直接执行 fmt.Println，而是先回到循环头部重新评估条件
 			// 正是使用 for 而非 if 的原因，确保在每次唤醒后都重新检查共享状态。
             for len(data) < datacap {
+				// 调用 Wait() 后内部会释放锁，并将该 goruntine 挂起并等待唤醒
+				// 当其他 goroutine 调用 cond.Broadcast() 唤醒它时，Wait() 会重新尝试获取锁（阻塞直到获得锁），获取锁成功后，Wait() 返回。
                 c.Wait()
             }
             fmt.Println("listen", len(data), time.Now())
