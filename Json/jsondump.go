26dd328e28452a364b5fef587b286600de865a96
Kagari22 <151036819+Kagari22@users.noreply.github.com>
2026-07-10 19:37:18 +0800

    Create jsondump.go

---
+++ Json/jsondump.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+type User struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func main() {
+	user := User{
+		Name: "张三",
+		Age: 25,
+	}
+
+	data, err := json.MarshalIndent(user, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	err = os.WriteFile("data.json", data, 0644)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	fmt.Println("写入成功")
+}
